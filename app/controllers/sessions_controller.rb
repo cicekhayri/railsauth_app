1cef4de8ae2e2eac413f5e2ed2e598b20ff407e2
Hayri <hayri@hoshilab.com>
2014-10-09 09:59:57 +0200

    check if session checks for right password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:email])
+
+    if user
+      if user.authenticate(params[:password])
+        render text: "PASSWORD OK"
+      else
+        render text: 'password not ok'
+      end
+    else
+      render text: 'user not found'
+    end
+  end
 end
