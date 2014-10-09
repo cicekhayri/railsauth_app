69b1a6b82338e0567cd8b06aae3c5f2565c53312
Hayri <hayri@hoshilab.com>
2014-10-09 10:07:33 +0200

    the session create action redirect if password and user is right

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
 
-    if user
-      if user.authenticate(params[:password])
-        render text: "PASSWORD OK"
-      else
-        render text: 'password not ok'
-      end
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to root_url
     else
-      render text: 'user not found'
+      render :new
     end
   end
 end
