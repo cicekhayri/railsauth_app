1f93a3dc000271df8f8620b262f91fd9bb7a3237
Hayri <hayri@hoshilab.com>
2014-10-09 11:16:02 +0200

    user registration implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
    
+  resources :users
+
   resources :sessions 
 
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'login', to: 'sessions#new', as: 'login'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
