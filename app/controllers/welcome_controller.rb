b3c46eed25bda3565c115a49874c7b2e9c737695
Hayri <hayri@hoshilab.com>
2014-10-09 10:58:46 +0200

    implemented the destroy session action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  
+   
   resources :sessions 
+
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
